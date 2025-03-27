ff0138fe0e2e3fb90e37bf9403a2877e3ccaee1a
vishnuviratGit <vishnuvirat564@gmail.com>
2025-03-27 13:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let count=0;
+document.getElementById("incrementBtn").addEventListener("click", function(){
+	 count++;
+	 document.getElementById("counter").textContent=count;
+	alert("Counter-value: "+ count);
+})
